2212d7658e0b81cb9d44eb49f5bc22675bd511ec
Casey <caseyderiso@gmail.com>
2020-12-20 20:55:39 -0600

    set up connection to sql database with sequelize

---
+++ config/connection.js
@@ -0,0 +1,12 @@
+// import the Sequelize constructor from the library
+const Sequelize = require('sequelize');
+require('gotenv').config();
+
+// create connection to our databse, pass in your MySQL information for user/pass
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  host: 'localhost',
+  dialect: 'mysql',
+  port: 3306
+});
+
+module.exports = sequelize;
