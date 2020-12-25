8bb45782846d804c66177b9ff1c7d05819ee21f9
Casey <caseyderiso@gmail.com>
2020-12-25 16:55:13 -0600

    included comments in post GET requests

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { Post, User, Vote } = require("../../models");
+const { Post, User, Vote, Comment } = require("../../models");
 const sequelize = require("../../config/connection");
 
 // get all users
@@ -20,6 +20,14 @@ router.get("/", (req, res) => {
     order: [["created_at", "DESC"]],
     include: [
       {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
         model: User,
         attributes: ["username"],
       },
@@ -33,8 +41,32 @@ router.get("/", (req, res) => {
 });
 
 router.get("/:id", (req, res) => {
-  // custom static method created in models/Post.js
-  Post.upvote(req.body, { Vote })
+  Post.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
     .then((dbPostData) => {
       if (!dbPostData) {
         res.status(404).json({ message: "No post found with this id!" });
