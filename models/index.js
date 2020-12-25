e709cd7407087fd2c2901a569bd469cc4ed948c5
Casey <caseyderiso@gmail.com>
2020-12-25 16:43:50 -0600

    created model and routes for comments

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
-const User = require("./user");
-const Post = require("./post");
-const Vote = require("./vote");
+const User = require("./User");
+const Post = require("./Post");
+const Vote = require("./Vote");
+const Comment = require("./Comment");
 
 // create associations
 User.hasMany(Post, {
@@ -23,7 +24,6 @@ Post.belongsToMany(User, {
   foreignKey: "post_id",
 });
 
-
 Vote.belongsTo(User, {
   foreignKey: 'user_id'
 });
@@ -40,4 +40,20 @@ Post.hasMany(Vote, {
   foreignKey: 'post_id'
 });
 
-module.exports = { User, Post, Vote };
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+})
+
+module.exports = { User, Post, Vote, Comment };
